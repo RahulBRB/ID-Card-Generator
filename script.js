20d5783996164afdbf38bb64823fc04f00be8192
Rahul Baskey <86495244+RahulBRB@users.noreply.github.com>
2023-10-17 20:08:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.getElementById("generate-button").addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
 
     const userName = document.getElementById("name").value;
     const userTitle = document.getElementById("title").value;
     const userLocation = document.getElementById("location").value;
     const userInfo = document.getElementById("info").value;
+    const userImageUrl = document.getElementById("image-url").value;
+    const userAbout = document.getElementById("about").value;
     const userImage = document.getElementById("image").files[0];
 
     if (userName !== "") {
@@ -29,5 +31,14 @@ document.getElementById("generate-button").addEventListener("click", function (e
             document.getElementById("user-image").src = reader.result;
         };
         reader.readAsDataURL(userImage);
+    } else if (userImageUrl) {
+        document.getElementById("user-image").src = userImageUrl;
+    }
+
+    if (userAbout !== "") {
+        if (userAbout.length > 200) {
+            userAbout = userAbout.substring(0, 200); // Truncate if longer than 200 characters
+        }
+        document.getElementById("user-about").textContent = userAbout;
     }
 });
